4a329660353beb2c545fcab9164d77a06ba946e8
ThiagoSCarv <thiagocarv4@gmail.com>
2025-07-17 20:43:28 -0300

    entendendo o contexto de cada teste

--- tests/testE2E.spec.ts
+++ tests/testE2E.spec.ts
@@ -11,14 +11,36 @@ describe('transactions', () => {
     await app.close()
   })
 
+  const server = app.server
+
   test("usuario pode criar uma transação", async () => {
-    const response = await request(app.server).post("/transactions").send({
+    const response = await request(server).post("/transactions").send({
       title: "New transaction",
       amount: 900,
       type: "credit"
     })
 
     expect(response.statusCode).toBe(201)
+  })
+
+  test('Deve retornar todas as transações do usuario', async () => {
+    const createTransaction = await request(server).post("/transactions").send({
+      title: "New transaction",
+      amount: 900,
+      type: "credit"
+    })
+
+    const cookies = createTransaction.get('Set-Cookie')
+
+    const listOfTransactions = await request(server).get("/transactions").set("Cookie", cookies).expect(200)
+
+    expect(listOfTransactions.body.transactions).toEqual([
+      expect.objectContaining({
+        title: "New transaction",
+        amount: 900,
+      })
+    ])
+
   })
 
 })
