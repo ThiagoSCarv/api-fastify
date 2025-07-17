2b54db99d2a4762824d1d325ffcba654606230eb
ThiagoSCarv <thiagocarv4@gmail.com>
2025-07-17 14:12:03 -0300

    rotas/controller index e show

--- src/routes/transactionsRoutes.ts
+++ src/routes/transactionsRoutes.ts
@@ -4,6 +4,24 @@ import { FastifyInstance } from "fastify";
 import { z } from "zod/v4";
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get("/", async (request, reply) => {
+    const transactions = await knex("transactions").select()
+
+    return { transactions }
+  })
+
+  app.get("/:id", async (request, reply) => {
+    const paramsSchema = z.object({
+      id: z.string(),
+    })
+
+    const { id } = paramsSchema.parse(request.params)
+
+    const transaction = await knex("transactions").select().where("id", id).first()
+
+    return { transaction }
+  })
+
   app.post("/", async (request, reply) => {
     const bodySchema = z.object({
       title: z.string().min(2),
